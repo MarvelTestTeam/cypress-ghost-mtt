1f820eeb5a3c92e75540f06b28422126846315c2
AfiffeRetamozo <afiffe.retamozo.a@gmail.com>
2022-05-09 00:07:01 -0500

    Escenario 14 Jorge

---
+++ cypress/integration/Pruebas_jorge/Escenario14.spec.js
@@ -0,0 +1,27 @@
+/// <reference types="cypress" />
+
+context('Actions', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:2369/ghost/#/signin')
+  })
+
+  // https://on.cypress.io/interacting-with-elements
+
+  it('.type() - type into a DOM element', () => {
+    // https://on.cypress.io/type
+    cy.get('input[name="identification"]').type('afiffe.retamozo.a@gmail.com')
+            cy.get('input[name="password"]').type('Asdfg12345!')
+            cy.get(('[id=ember11]')).click()
+            cy.contains(('Tags')).click()
+            cy.get(('.gh-tag-list-name')).click()
+            cy.get(('.input-color > .gh-input')).type('tabhti')
+            cy.get(('[id=ember66]')).click()
+            
+            
+          
+            
+            
+  })
+
+
+})
